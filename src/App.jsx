188bf2a35831720f397b4f7174a65bb21aa4aea5
grasit <idongesiteshiet980@gmail.com>
2023-01-16 12:17:25 +0100

    made few changes, edited read-me

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { baseTheme, Box, Card, CardBody, Circle, Container, Divider, Grid, Heading, HStack, Stat, StatGroup, StatHelpText, StatLabel, StatNumber} from "@chakra-ui/react"
+import { Box, Card, CardBody, Circle, Container, Divider, Grid, Heading, HStack, Stat, StatGroup, StatHelpText, StatLabel, StatNumber} from "@chakra-ui/react"
 import Chart from "../Components/Chart"
 
 export default function App() {
